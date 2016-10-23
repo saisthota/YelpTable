1b5c978a0f351d456e919a84d7a7260d636c0d75
saisthota <me@saisthota.com>
2016-10-23 11:05:05 -0700

    Browser Click action

--- extension/reviews.js
+++ extension/reviews.js
@@ -1,3 +1,12 @@
-/**
- * Created by saisthota on 10/23/16.
- */
+chrome.browserAction.onClicked.addListener(function(tab) {
+
+            chrome.tabs.sendMessage(tab.id, {
+                    command: "change_title",
+                    title: "hoge"
+                },
+                function(msg) {
+                    console.log("result message:", msg);
+                });
+
+});
+
