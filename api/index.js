ba33e6a65bbe2ccc1f6ddf2aa99d7f07c1eaae34
saisthota <me@saisthota.com>
2016-10-23 02:21:38 -0700

    Using Node-Yelp Module

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var Yelp = require('yelp');
 var request = require('request');
-var oauth   = require('oauth-signature');
-var nonce  = require('nonce')();
+
 var config = require('./config');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,35 +19,18 @@ router.get('/', function(req, res) {
 
 router.get('/restaurant/:name', function(req, res) {
 
-    var url = 'http://api.yelp.com/v2/search';
+    console.log(req.params.name);
+    var yelp = new Yelp({
+        consumer_key: config.getConsumerKey(),
+        consumer_secret: config.getConsumerSecret(),
+        token: config.getToken(),
+        token_secret: config.getTokenSecret()
+    });
 
-    var def_params = {
-        location: 'San+Francisco',
-        sort: '2'
-    };
-
-    var req_params = {
-        oauth_consumer_key: config.getConsumerKey(),
-        oauth_token: config.getToken(),
-        oauth_nonce: nonce(),
-        oauth_timestamp: nonce().toString().substr(0, 10),
-        oauth_signature_method: 'HMAC-SHA1',
-        oauth_version: '1.0'
-    };
-
-    var params = _.assign(def_params, set_parameters, req_params);
-
-    var cosumerSecret = config.getConsumerSecret();
-    var tokenSecret = config.getTokenSecret();
-
-    var signature = oauth.generate('GET', url, params, consumerSecret, tokenSecret, {encodeSignature: false});
-
-    params.oauth_signature = signature;
-
-    var paramsURL  = qs.stringify(params);
-
-
-
+    yelp.business('prime-santa-clara', function(err, data) {
+        if (err) return console.log(error);
+            res.json(data.reviews);
+    });
 
 });
 
