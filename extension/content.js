65907b86556751942e15df629a7172bf0da78a74
saisthota <me@saisthota.com>
2016-10-23 11:04:44 -0700

    Added DOM manipulation

--- extension/content.js
+++ extension/content.js
@@ -1,3 +1,20 @@
-/**
- * Created by saisthota on 10/23/16.
- */
+/* Listen for messages */
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    if (msg.command && (msg.command == "change_title")) {
+        var biz = $('.page-header-title').text();
+
+        var http = new XMLHttpRequest();
+        var url = "http://127.0.0.1:8080/api/restaurant/"+biz;
+        http.open("GET", url, true);
+
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+
+
+                $("#reservation").html(msg.title);
+                sendResponse("the page title's changed: '" + src + "' -> '" + dst + "'");
+            }
+        }
+        http.send();
+    }
+});
