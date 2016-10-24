466219a722a2c78d0ba045d284ffa20ea5affb98
saisthota <me@saisthota.com>
2016-10-23 23:47:56 -0700

    Updated DOM manipulation

--- extension/content.js
+++ extension/content.js
@@ -2,15 +2,16 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.command && (msg.command == "change_title")) {
         var biz = $('.page-header-title').text() + ' ' + $( "ul.profile-header-meta-items  li:nth-child(2)" ).text();
+        $("#reviews-summary h3").append(" from Yelp");
 
         var http = new XMLHttpRequest();
-        var url = "http://127.0.0.1:8080/api/restaurant/"+biz+" santa clara";
+        var url = "http://127.0.0.1:8080/api/restaurant/"+biz;
         http.open("GET", url, true);
 
         http.onreadystatechange = function() {
             if (http.readyState == 4 && http.status == 200) {
                 $("#reviews").html(http.responseText);
-                sendResponse("the page title's changed: '" + src + "' -> '" + dst + "'");
+                sendResponse("Loading reviews");
             }
         }
         http.send();
