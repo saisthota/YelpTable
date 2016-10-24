6680f62da4d2b2dbd82a9c07202b37cfc0979bad
saisthota <me@saisthota.com>
2016-10-23 23:56:54 -0700

    Refactoring

--- extension/content.js
+++ extension/content.js
@@ -4,13 +4,17 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
         var biz = $('.page-header-title').text() + ' ' + $( "ul.profile-header-meta-items  li:nth-child(2)" ).text();
         $("#reviews-summary h3").append(" from Yelp");
 
+
+
         var http = new XMLHttpRequest();
         var url = "http://127.0.0.1:8080/api/restaurant/"+biz;
         http.open("GET", url, true);
 
         http.onreadystatechange = function() {
             if (http.readyState == 4 && http.status == 200) {
-                $("#reviews").html(http.responseText);
+                $("#reviews-results").html(http.responseText);
+                $("#reviews-toolbar").remove();
+                $("#review-pagination").remove();
                 sendResponse("Loading reviews");
             }
         }
